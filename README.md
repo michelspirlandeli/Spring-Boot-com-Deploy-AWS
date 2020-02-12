ba7e636659b2820763e6894eeab4a1bb71ac91a7
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-02-11 21:35:18 -0300

    update

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Michel Spirlandeli
 [https://github.com/michelspirlandeli]
 
 ## Contributing
+
+
+## :memo: License
+This project is under the MIT license. See the [LICENSE](https://github.com/michelspirlandeli/Spring-Boot-com-Deploy-AWS/blob/master/LICENSE) for more information.
--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -4,11 +4,15 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -16,6 +20,11 @@ public class CodeBlogController {
 
     @Autowired
     CodeBlogService codeBlogService;
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
     public ModelAndView getPosts(){
@@ -31,5 +40,21 @@ public class CodeBlogController {
         Post post = codeBlogService.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @RequestMapping(value="/newpost", method=RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @RequestMapping(value="/newpost", method=RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeBlogService.save(post);
+        return "redirect:/posts";
     }
 }
